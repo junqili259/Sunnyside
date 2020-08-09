515889e287247de4b1325b9b25fc4e9b5958784d
junqili <junqili259@gmail.com>
2020-08-09 18:37:03 -0400

    Junqili259 patch 1 (#4) (#6)
    * Setup fixes (#3)
    
    * changed folder name
    
    * Create LICENSE
    
    * setup update
    
    * Create python-publish.yml

--- setup.py
+++ setup.py
@@ -15,9 +15,9 @@ setup(
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      
+    'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8'
