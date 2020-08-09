fbad0396b4aa2cb55442b8cf39e07f5fb0b5512c
junqili <junqili259@gmail.com>
2020-08-09 18:32:53 -0400

    Setup fixes (#5)
    * changed folder name
    
    * Create LICENSE
    
    * setup update
    
    * Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 from distutils.core import setup
 setup(
-  name = 'sunnyside',         # How you named your package folder (MyLib)
-  packages = ['sunnyside'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  name = 'sunnyside',        
+  packages = ['sunnyside'],   
+  version = '0.1',     
+  license='MIT',        
   description = 'Python wrapper for OpenWeather API',   # Give a short description about your library
-  author = 'Jun Qi Li',                   # Type in your name
-  author_email = 'JunQi.Li63@myhunter.cuny.edu',      # Type in your E-Mail
-  url = '',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  author = 'Jun Qi Li',                   
+  author_email = 'JunQi.Li63@myhunter.cuny.edu',      
+  url = '',   
+  download_url = 'https://github.com/junqili259/Sunnyside/archive/v0.1.tar.gz',   
   keywords = ['OpenWeather'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+  install_requires=[            
           'requests'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Intended Audience :: Developers',      
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'License :: OSI Approved :: MIT License',   
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Python :: 3.7',
     'Programming Language :: Python :: 3.8'
