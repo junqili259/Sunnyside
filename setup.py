4753b8f7311fdb1dac6d300aa4d2da6e3a09f5ef
junqili <junqili259@gmail.com>
2020-08-10 06:03:05 -0400

    Bug fixes (#13)
    * fix for None
    
    * updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="sunnyside",
-    version="1.0.0",
+    version="1.0.1",
     author="Jun Qi Li",
     author_email="JunQi.Li63@myhunter.cuny.com",
     description="Python wrapper for OpenWeather API",
