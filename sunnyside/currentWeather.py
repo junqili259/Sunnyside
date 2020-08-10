c0033a5a83df9b8f0dd516e7b26895f99ea06579
junqili <junqili259@gmail.com>
2020-08-10 05:13:12 -0400

    added 5 day weather forecast support
    * Update .gitignore
    
    * added units
    
    * Update README.md
    
    * Update README.md
    
    * added 5 day forecast support
    
    * Update README.md
    
    * Update README.md

--- sunnyside/currentWeather.py
+++ sunnyside/currentWeather.py
@@ -2,14 +2,19 @@ import requests
 
 
 class CurrentWeather:
-    def __init__(self, api_key,):
+    def __init__(self, api_key, units = None):
         self.api_key = api_key
+        self.units = units
 
     def get_current_weather_by_city_name(self, city_name):
         """
         Returns with a list of weather parameters by city name
         """
-        api_url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={self.api_key}"
+        if self.units == None:
+            api_url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={self.api_key}"
+        else:
+            api_url = f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={self.api_key}" + f"&units={self.units}"
+
         response = requests.get(url=api_url)
         return response.json()
 
@@ -19,18 +24,26 @@ class CurrentWeather:
         Args:
             city_id: Int
         """
-        api_url = f"https://api.openweathermap.org/data/2.5/weather?id={city_id}&appid={self.api_key}"
+        if self.units == None:
+            api_url = f"https://api.openweathermap.org/data/2.5/weather?id={city_id}&appid={self.api_key}"
+        else:
+            api_url = f"https://api.openweathermap.org/data/2.5/weather?id={city_id}&appid={self.api_key}" + f"&units={self.units}"
+
         response = requests.get(url=api_url)
         return response.json()
     
-    def get_current_weather_by_geographic_coord(self, lat, lon):
+    def get_current_weather_by_geo_coords(self, lat, lon):
         """
         Returns with a list of weather parameters by geo coord
         Args:
             lat: Int
             lon: Int
         """
-        api_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={self.api_key}"
+        if self.units == None:
+            api_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={self.api_key}"
+        else:
+            api_url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={self.api_key}" + f"&units={self.units}"
+
         response = requests.get(url=api_url)
         return response.json()
 
@@ -41,6 +54,10 @@ class CurrentWeather:
             zip_code: Int
             country_code: Optional
         """ 
-        api_url = f"https://api.openweathermap.org/data/2.5/weather?zip={zip_code},{country_code}&appid={self.api_key}"
+        if self.units == None:
+            api_url = f"https://api.openweathermap.org/data/2.5/weather?zip={zip_code},{country_code}&appid={self.api_key}"
+        else:
+            api_url = f"https://api.openweathermap.org/data/2.5/weather?zip={zip_code},{country_code}&appid={self.api_key}" + f"&units={self.units}"
+            
         response = requests.get(url=api_url)
         return response.json()
